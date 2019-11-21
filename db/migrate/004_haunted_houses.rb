3cf35e6d87d79a957e2472544fd73203eb23fec6
Brian Pompey <brianthescholar@gmail.com>
2019-11-21 04:08:46 +0000

    Automatically backed up by Learn

--- db/migrate/004_haunted_houses.rb
+++ db/migrate/004_haunted_houses.rb
@@ -1,5 +1,5 @@
-class FixHauntedHouseDescription < ActiveRecord::Migration[5.2]
-  def change
-    change_column :haunted_houses, :description, :text
-  end
-end
+#class FixHauntedHouseDescription < ActiveRecord::Migration[5.2]
+#  def change
+#    change_column :haunted_houses, :description, :text
+#  end
+#end
